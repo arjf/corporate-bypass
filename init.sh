a77a86827c39f750db5a433e27d8fd4222657724
ajdjyt <ajdjking@gmail.com>
2025-04-02 14:46:32 +0530

    improve init.sh : exit on any error, fix variable string encapsulation handling, add more checks, fix .env file locating issue

--- init.sh
+++ init.sh
@@ -1,30 +1,77 @@
 #!/bin/sh
+set -e # Exit if any command exits non zero
 
 # Import variables from .env
+if [ ! -f ./.env ]; then
+    echo "Error: .env file is missing"
+    exit 1
+fi
+
 set -a
-. .env
+. ./.env
 set +a
 
-if [ -z $vols ]; then
-    printf "\$vols is not declared in the environment."
+# Variable checks
+if [ -z "$vols" ]; then
+    echo "Error: \$vols is not declared in the environment."
+    exit 1
+fi
+
+if [ -z "$DOMAIN" ]; then
+    echo "Error: \$DOMAIN is not declared in the environment."
+    exit 1
+fi
+
+if [ -z "$CERTBOT_EMAIL" ]; then
+    echo "Warning: \$CERTBOT_EMAIL is not set. Proceeding without email."
+fi
+
+# Command checks
+if ! command -v docker >/dev/null 2>&1; then
+    echo "Error: Docker is missing"
+    exit 1
+fi
+
+if ! docker compose version >/dev/null 2>&1; then
+    echo "Error: Docker Compose is missing"
+    exit 1
+fi
+
+# Check if required files exist
+if [ ! -f nginx/funnel.conf ]; then
+    echo "Error: nginx/funnel.conf missing"
+    exit 1
+fi
+
+if [ ! -f nginx/99-autoreload.sh ]; then
+    echo "Error: nginx/99-autoreload.sh missing"
+    exit 1
 fi
 
 # Create folders and copy files
-mkdir -p $vols/certbot/www
-mkdir -p $vols/certbot/conf
-mkdir -p $vols/nginx/entrypoints
-mkdir -p $vols/nginx/conf
-cp nginx/funnel.conf $vols/nginx/conf/
-cp nginx/99-autoreload.sh $vols/nginx/entrypoints/
+echo "Creating folders"
+mkdir -p "$vols/certbot/www"
+mkdir -p "$vols/certbot/conf"
+mkdir -p "$vols/nginx/entrypoints"
+mkdir -p "$vols/nginx/conf"
+cp nginx/funnel.conf "$vols/nginx/conf/"
+cp nginx/99-autoreload.sh "$vols/nginx/entrypoints/"
+chmod +x "$vols/nginx/entrypoints/99-autoreload.sh"
+
+# If single domain set array to single doamin
+if [ -z "$domains" ]; then
+    domains="$DOMAIN"
+fi
 
 # Certbot setup
+echo "Setting up certbot"
 rsa_key_size=4096
 data_path="$vols/certbot/"
 email="$CERTBOT_EMAIL"
 staging=0
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-    echo "### Downloading recommended TLS parameters ..."
+    echo "Downloading recommended TLS parameters"
     mkdir -p "$data_path/conf"
     curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf >"$data_path/conf/options-ssl-nginx.conf"
     curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem >"$data_path/conf/ssl-dhparams.pem"
@@ -57,7 +104,7 @@ echo
 
 # Any domain arguments if required
 domain_args=""
-for domain in "${domains[@]}"; do
+for domain in $domains; do
     domain_args="$domain_args -d $domain"
 done
 
@@ -68,7 +115,7 @@ case "$email" in
 esac
 
 # Set staging arg
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+if [ "$staging" != "0" ]; then staging_arg="--staging"; fi
 
 # Get letsencrypt certs
 echo "Obtaining letsencrypt certificates"
