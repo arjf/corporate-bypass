50b84152032f4a0c7d989889ed4ee807e3b8e303
ajdjyt <ajdjking@gmail.com>
2025-04-02 15:57:38 +0530

    fix typo in 98-envsubst.sh nginx.conf -> funnel.conf

--- nginx/98-envsubst.sh
+++ nginx/98-envsubst.sh
@@ -1,2 +1,2 @@
-envsubst '${DOMAIN}' < /etc/nginx/conf/nginx.conf.template > /etc/nginx/conf/funnel.conf
+envsubst '${DOMAIN}' < /etc/nginx/conf/funnel.conf.template > /etc/nginx/conf/funnel.conf
 nginx -g 'daemon off;'
